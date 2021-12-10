ad43401af0925c19f70b2f0b7303d533314d0b01
Yuta123456 <yuuta09090530@icloud.com>
2021-12-10 12:56:15 +0900

    itemが一個しかないときにうまくいかないバグを修正

--- util/make_coodinates.py
+++ util/make_coodinates.py
@@ -1,15 +1,14 @@
 import itertools
-
+import copy
 
 def make_coodinates(select_items, item, layer):
     coodinates = []
+    select_items = copy.deepcopy(select_items)
+    select_items[layer] = [item]
     k = [list(range(len(x))) for x in select_items]
     for selected_num_array in itertools.product(*k):
         coodinate = []
         for i, selected_num in enumerate(selected_num_array):
-            if i == layer:
-                coodinate.append(item)
-                continue
             coodinate.append(select_items[i][selected_num])
         coodinates.append(coodinate)
     return coodinates
