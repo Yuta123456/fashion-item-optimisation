127a6eb5562bf4fa4d5004b844a8db572251b825
Yuta123456 <yuuta09090530@icloud.com>
2022-01-03 19:23:31 +0900

    相関係数見るためのプログラム

---
+++ scripts/corr.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+def read_data(path):
+    with open(path, mode="r") as f:
+        res = [float(s.strip()) for s in f.readlines()]
+    return res
+com = read_data('data/com.txt')
+ver = read_data('data/ver.txt')
+sim = read_data('data/sim.txt')
+mul = read_data('data/mul.txt')
+
+N = len(com)
+df = pd.DataFrame(index=['i'+str(i) for i in range(N)])
+df['com'] = com
+df['ver'] = ver
+df['sim'] = sim
+df['mul'] = mul
+print(df.describe())
+corr = df.corr()
+print(corr)
