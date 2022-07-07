c56225fd750a6b40f9120f61370518f8d22e76cf
Yuta123456 <yuuta09090530@icloud.com>
2022-07-07 20:48:21 +0900

    実験用プログラム作成

--- experiments/main.py
+++ experiments/main.py
@@ -35,35 +35,37 @@ similarity_model.load_state_dict(torch.load('model.pth'))
 model = ScoreEstimater(topic_model, all_items, similarity_model)
 
 COUNT = 500
-model = ScoreEstimater(topic_model, [[], [], []], similarity_model)
 
 def optimization():
     average_score = [0, 0, 0, 0]
     for cnt in range(COUNT):
         select_items = init_closet(all_items, TIME_STEP)
-        model.set_all_items(all_items)
         delta_obj = EPSILON + 1
-        pre_obj = 0
         # ここから最適化
+        obj = []
         while delta_obj >= EPSILON:
             for layer in range(LAYER):
                 # 対象のレイヤを空に
                 select_items[layer] = []
                 for _t in range(TIME_STEP):
                     # sigmaはその際の増加分
-                    _, additional_item = select_max_incremental_item(select_items, all_items,layer, model)
+                    sigma, additional_item = select_max_incremental_item(select_items, all_items,layer, model)
                     # 増加分が最大となるアイテムを追加
                     select_items[layer].append(additional_item)
+                obj.append(sigma)
             # 今回のループでの最適値はobj[LAYER-1][0]に格納されている。
             cur_obj = sum(calc_closet_score(select_items, model))
-            # print(calc_closet_score(select_items, model))
             delta_obj = cur_obj - pre_obj
             pre_obj = cur_obj
-            # print(f"{roop_cnt}回目のループでの増加分{delta_obj}")
 
         score = calc_closet_score(select_items, model)
-
         print("com:{:.2f} ver:{:.2f} sim:{:.2f} mul:{:.2f}".format(*score))
         cnt += 1
-        if (cnt % 5 == 0):
+        if cnt % 5 == 0:
             print(f"{cnt * 100 / COUNT}% 終了しました")
+        for i in range(len(average_score)):
+            average_score[i] += score[i]
+    average_score = list(map(lambda x: x/COUNT, average_score))
+    return average_score
+# 重みを変更させながら実行する必要がある。
+optimization()
